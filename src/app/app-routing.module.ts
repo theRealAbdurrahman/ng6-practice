4440b89ab15c61ebacf4f273be800a153e4709a2
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-06-26 16:25:32 +0200

    modified routes and generated data service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'details/:id', component: DetailsComponent },
   { path: 'posts', component: PostsComponent },
-  { path: 'posts', component: UsersComponent },
-  { path: 'posts', component: DetailsComponent },
 ];
 
 @NgModule({
