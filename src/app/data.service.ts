45eb22f9fd4f879623b2b7b23f71c4091364d1fa
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-06-26 16:44:40 +0200

    fixed some styling errors

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  baseurl: string = 'https://jsonplaceholder.typicode.com'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getusers() {
+    return this.http.get(`${this.baseurl}/users`)
+  }
 }
