d76d010847de2c223e3fd903c4c90319491677ba
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-06-27 12:29:40 +0200

    added bootstrap

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.scss']
 })
 export class CreatePostComponent implements OnInit {
-  post: object = {
+  post: Object = {
     title: '',
     body: '',
   }
